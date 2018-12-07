c9fe14cd035e7d96d8c8917ef345d5cf15e99ad2
Andrew Johnson <andrew.johnson612@gmail.com>
2018-12-07 14:45:06 -0600

    Changed the layout with Semantic UI and improved functionality

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -5,19 +5,15 @@ import VideoItem from './VideoItem'
 const VideoList = ({ videos, onVideoSelect }) => {
 	const renderedList = videos.map(video => {
 		return (
-			<div className="ui relaxed divided list">
 				<VideoItem 
 					video={video} 
 					onVideoSelect={onVideoSelect} 
 					key={video.id.videoId}	
 				/>
-			</div>
 		)
 	})
 
-	console.log(renderedList)
-	console.log(videos)
-	return <div>{renderedList}</div>
+	return <div className="ui relaxed divided list">{renderedList}</div>
 }
 
 
